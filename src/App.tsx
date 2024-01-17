fc56867d4acf96ff8e01ad9eac719d60daddfead
mehra-sourav <1997mehra@gmail.com>
2024-01-17 21:42:57 +0530

    Created login and signup UI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import LoginForm from "@/components/LoginForm";
+import SignupForm from "./components/SignupForm";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <LoginForm /> */}
+      <SignupForm />
+    </>
+  );
+}
+
+export default App;
