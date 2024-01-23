d2f434123350705a21f31a8e4feb4e0d480dd6bb
mehra-sourav <1997mehra@gmail.com>
2024-01-23 20:04:08 +0530

    Testing workflow file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Chart from "@/components/Chart";
+// import Chart from "@/components/Chart";
+import LoginForm from "@/components/LoginForm";
 
 function App() {
   return (
     <>
-      <Chart maxHeight="30%" maxWidth="30%" />
+      {/* <Chart maxHeight="30%" maxWidth="30%" /> */}
+      <LoginForm />
     </>
   );
 }
